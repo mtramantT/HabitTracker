ff8d8c1ff5085cd92f0090468d8d9e7ec2f5a71a
Matthew Tramantano <c-matthew.tramantano@charter.com>
2021-07-24 18:06:15 -0600

    React + Typescript template added, Spring boot Rest API added

---
+++ root/habit-api/Habit-API/src/main/java/com/portal/HabitApiApplication.java
@@ -0,0 +1,13 @@
+package com.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitApiApplication.class, args);
+	}
+
+}
