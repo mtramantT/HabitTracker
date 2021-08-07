070a2256d255bb62f1e0d9b89eee847e11c081aa
Matthew Tramantano <c-matthew.tramantano@charter.com>
2021-08-07 15:15:36 -0600

    Changed method 'one' to 'findOne', Added EntityModel with links to 'newTracker', added EntityModel to 'findOne', created findAll method and added CollectionModel to return

--- root/habit-api/Habit-API/src/main/java/com/portal/controller/TrackerController.java
+++ root/habit-api/Habit-API/src/main/java/com/portal/controller/TrackerController.java
@@ -1,8 +1,14 @@
 package com.portal.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,15 +33,29 @@ public class TrackerController {
 	}
 	
 	@PostMapping("/employees")
-	public Tracker newTracker(@RequestBody Tracker tracker) {
-		return repo.save(tracker);
+	public EntityModel<Tracker> newTracker(@RequestBody Tracker tracker) throws TrackerNotFoundException {
+		Tracker newTracker = repo.save(tracker);
+		return EntityModel.of(newTracker, //
+				linkTo(methodOn(TrackerController.class).newTracker(tracker)).withSelfRel(),
+				linkTo(methodOn(TrackerController.class).findOne(tracker.getId())).withRel("Tracker"));
 	}
 	
 	@GetMapping("/trackers/{id}")
-	public Tracker one(@PathVariable Long id) throws TrackerNotFoundException {
-		return repo.findById(id)
+	public EntityModel<Tracker> findOne(@PathVariable Long id) throws TrackerNotFoundException {
+		Tracker updated = repo.findById(id)
 				.orElseThrow(() -> new TrackerNotFoundException(id));
+		return EntityModel.of(updated, //
+				linkTo(methodOn(TrackerController.class)).withSelfRel());
 	}
 	
+	@GetMapping("/trackers/trackers")
+	public CollectionModel<EntityModel<Tracker>> findAll() {
+		List<Tracker> list = repo.findAll();
+		List<EntityModel<Tracker>> entityCollection = 
+				list.stream().map(model -> 
+				EntityModel.of(model, linkTo(methodOn(TrackerController.class).findAll()).withSelfRel()))
+					.collect(Collectors.toList()); 
+		return CollectionModel.of(entityCollection);
+	}
 	
 }
